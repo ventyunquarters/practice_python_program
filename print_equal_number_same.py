6bb7f959745b1ba627e28709ea759e707c0ace86
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 00:50:36 +0800

    Final file work.

--- print_equal_number_same.py
+++ print_equal_number_same.py
@@ -1,5 +1,9 @@
 #Ask the user to input two numbers
-
+def prog02():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
 #Condition where if num1 is equal to num2
-
+    if num1 == num2:
+        print("equal")
 #Print the equal result and call the function
+prog02()
