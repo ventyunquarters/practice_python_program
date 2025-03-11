fe7198a4031f6ee3284ee44f1f7786ace0075231
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-11 20:39:27 +0800

    Program one batch 1 Final

--- prog01_print_bigger_number.py
+++ prog01_print_bigger_number.py
@@ -1,5 +1,9 @@
 #Ask user to input two number
 
+def prog01():
+    num1= int(input("Enter the first number: "))
+    num2= int(input("Enter the second number: "))
 #Get the max between num1 and num2
-
-#print max result
+    print(max(num1, num2))
+#call function to print bigger number
+prog01()
