9eaddeb517ddced3336efec11113074cdd8fff87
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 23:44:34 +0800

    Added the range

--- print_sum_all_numbers.py
+++ print_sum_all_numbers.py
@@ -1,7 +1,8 @@
 #Define program to number 7
-
+def prog07():
 #Set the total sum to 0
-
+    total_sum = 0
 #Set a range for the 10 numbers
-
+    for i in range(10):
+        num = int(input(f"Enter a number {i+1}: "))
 #Calculate the total sum
