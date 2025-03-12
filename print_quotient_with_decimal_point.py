68ad9bbaa7e4c8e6642f0d4911b4ad6a3d33c3d2
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 22:34:03 +0800

    Added if condition

--- print_quotient_with_decimal_point.py
+++ print_quotient_with_decimal_point.py
@@ -1,7 +1,10 @@
 #Ask user to input two numbers
-
+def prog05():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 #Print cannot be divide by zero if num2 is equal to 0
-
+    if num2 == 0:
+        print("Cannot divide by zero")
 #Print the quotient
 
 #Call the function
