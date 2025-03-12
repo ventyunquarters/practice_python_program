3de6562ac3c201caf0bae505578dad3b20a9c1f1
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 23:59:32 +0800

    Added prompt user to enter a num

--- print_odd_number_count.py
+++ print_odd_number_count.py
@@ -1,6 +1,11 @@
 #Initialize a variable to count odd numbers starting to 0
-
+def prog08():
+    odd_count = 0
 #Prompt the user to enter a number
-
+    for i in range(10):
+        num = int(input(f"Enter a number {i +1}: "))
+        if num % 2 != 0:
+            odd_count += 1
 #Print the number of odd numbers
+
 
