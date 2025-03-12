b05712e8fee28ee45683a7fab90dd5050cbabce4
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 23:03:50 +0800

    Added result and print

--- print_firstnum_raised_to_secnum.py
+++ print_firstnum_raised_to_secnum.py
@@ -1,5 +1,10 @@
 #Ask user to input two numbers
-
+from unittest import result
+def prog06():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 #Set first number to be raised to second number
-
+    result = num1 ** num2
 #Print result
+    print(result)
+prog06()
