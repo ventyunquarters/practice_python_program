592c3bfa53e2444e27f9d1d65478f0a519f35d5c
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 22:15:54 +0800

    Added print product

--- print_product_two_numbers.py
+++ print_product_two_numbers.py
@@ -1,7 +1,9 @@
 #Asks user to input two numbers
-
+def prog04():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 #Calculate the product of num1 and num2
-
+    product = num1 * num2
 #Print the product
-
+    print(product)
 #Call the function of the program
