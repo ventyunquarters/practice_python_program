bba3412a5aac5579d160de9f80afdaac2a7d9c15
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-12 22:00:03 +0800

    With calculate of sum numbers

--- print_sum_two_numbers.py
+++ print_sum_two_numbers.py
@@ -1,7 +1,8 @@
 #Ask user to input two numbers
-
+def prog03():
 #Calculate the sum of num1 and num2
-
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 #Print the result of two numbers
-
+    sum_of_numbers = num1 + num2
 #Call the function to execute the code
