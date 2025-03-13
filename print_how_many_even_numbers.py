4ab0619b57bc92fce1ebf1929f185a04842116d2
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 00:35:00 +0800

    With loop

--- print_how_many_even_numbers.py
+++ print_how_many_even_numbers.py
@@ -1,7 +1,9 @@
 #Initialize variable to count even numbers. Start at 0
-
+def prog07():
+    even_count = 0
 #Loop 10 times to get 10 numbers from the user.
-
+    for i in range(10):
+        num = int(input(f"Enter a number {i + 1}: "))
 #Check if the number is even
 
 #Print the total count of even numbers,
