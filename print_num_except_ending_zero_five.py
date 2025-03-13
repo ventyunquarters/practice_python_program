e513b7f3189a9fb451504191929c763b9142535c
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 02:36:19 +0800

    Final file

--- print_num_except_ending_zero_five.py
+++ print_num_except_ending_zero_five.py
@@ -1,4 +1,9 @@
 # Define program and loop through numbers from 0 to 100
+def prog09():
+    for num in range (101):
+#Check if the number does NOT end with 0 and 5
+        if num % 10 != 0 and num % 10 != 5:
+            print(num)
 
-# Check if the number does NOT end with 0 and 5
+prog09()
 
