c823971d401e2d63f302d1bb8f5f4222f4d4e5b2
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-13 14:05:40 +0800

    If statement

--- print_not_equal.py
+++ print_not_equal.py
@@ -1,6 +1,8 @@
 #Ask user to input two numbers
-
-#Check if the num1 is equal to num2
-
+def prog02():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+#Check if the num1 is not equal to num2
+    if num1 != num2:
 #Set print not equal if they are not
 
