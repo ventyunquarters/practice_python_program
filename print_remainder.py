b1dff620358106eba3fb54063d46a77512ddc70a
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-13 17:18:21 +0800

    Initial commit

---
+++ print_remainder.py
@@ -0,0 +1,11 @@
+#Initialize an empty list to store the numbers
+
+#Loop 10 times to get 10 numbers from the user.
+
+#Get input and convert it to an integer.
+
+#Add number to the list
+
+#Initialize result with the first number on the list
+
+#Print final result
