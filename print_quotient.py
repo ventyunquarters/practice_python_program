95df74ce26bd82474a9fa4cd637aa7fcc4f2603b
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-13 15:40:13 +0800

    With if statement

--- print_quotient.py
+++ print_quotient.py
@@ -1,7 +1,10 @@
 #Ask the user to input two numbers
-
+def prog04():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 #Check if the second number is zero
-
+    if num2 == 0:
+        print("Cannot divide by zero.")
 #Calculate the integer quotient using floor division
 
 #Print the integer quotient
