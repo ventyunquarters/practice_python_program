902c55ce5aec63d00dca3356f8dec6b215bcf74c
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-13 23:36:30 +0800

    Initial commit

---
+++ print_remainder.py
@@ -0,0 +1,10 @@
+#Get user to input two numbers
+def prog05():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+# Check if the second number is zero
+    if num2 == 0:
+        print("Cannot divide by zero.")
+#Else calculate the remainder using the modulo operator
+
+
