3b407e4dcb34e3e4e47746b15bba6f33b0882509
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 02:29:17 +0800

    Added the print odd num

--- print_all_odd_num.py
+++ print_all_odd_num.py
@@ -1,8 +1,10 @@
 #Initialize the starting number to  1
-
+def prog08():
+    num = 1
 #Loop as long as the number is less than or equal to 100
-
+    while num <= 100:
 #Print the current odd number
-
+        print(num)
+        num += 2
 #Increment the number by 2 to get the next odd number
-
+prog08()
