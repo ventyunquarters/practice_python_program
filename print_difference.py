d7297c538442529d885a77ab7cd8ad0385467015
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-13 14:12:24 +0800

    With the algorithm

--- print_difference.py
+++ print_difference.py
@@ -1,8 +1,9 @@
 #Ask user to input number
-
+def prog03():
 #Calculate the difference between the two numbers
-
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 #Print the difference
-
+    difference = num1 - num2
 #Call the function
 
