774e3155a521c7fe50d0b1f8a07de9d0c12a1e1b
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 02:14:18 +0800

    Initial input

---
+++ print_all_odd_num.py
@@ -0,0 +1,8 @@
+#Initialize the starting number to  1
+
+#Loop as long as the number is less than or equal to 100
+
+#Print the current odd number
+
+#Increment the number by 2 to get the next odd number
+
