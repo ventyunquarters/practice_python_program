8007d4d77b032fb4b90f92e4ccdb8aba7fb00a2f
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 02:42:42 +0800

    Pseudocode

---
+++ print_all_num_between_two_num.py
@@ -0,0 +1,7 @@
+#Ask the user input two numbers
+
+# Check if num1 is less than num2, print numbers from num1+1 to num2-1
+
+# Check if num2 is less than num1, print numbers from num2+1 to num1-1
+
+# If the numbers are equal, there are no numbers between them.
