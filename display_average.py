f0e2afcbbea61e2abcfbca9cbdaa82089d458d16
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-15 01:15:24 +0800

    ASk user for input added

--- display_average.py
+++ display_average.py
@@ -1,9 +1,15 @@
 #Initialize an empty list to  store numbers
-
+def prog05():
+    numbers = []
 #Loop indefinitely until an invaalid input is given
-
+    while True:
+        try:
 #Get input and convert it to float.
-
+            num = float(input("Enter a number (or non-numeric to stop): "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Ending program.")
+            break
 #Check if the list of numbers is not empty.
 
 #Calculate the average
