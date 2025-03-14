acdd81adc1e7c028c3b04190ac8bd42f030f87ff
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-15 01:04:47 +0800

    Ask input

--- sort_num_highest_to_lowest.py
+++ sort_num_highest_to_lowest.py
@@ -1,8 +1,14 @@
 def prog04():
 #Add empty list to store the numbers.
-
+    numbers = []
 #Add loop until an invalid input is given
-
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Ending program.")
+        break
 #Get input and convert it to an integer.
 
 #Check the list of number is not empty.
