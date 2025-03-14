ea236d7af260847c76eeaa1d3ee90d6a388ee5b7
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 23:51:21 +0800

    Initial commit

---
+++ display_num_most_duplicated.py
@@ -0,0 +1,8 @@
+#Initialize an empty list to store the numbers
+
+#Loop indefinitely until an invalid input is given
+
+#Check if the list of numbers is empty
+
+#Initialize dictionary to store the count of each numbers
+
