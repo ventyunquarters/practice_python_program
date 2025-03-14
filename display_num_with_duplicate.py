51f7b7217468927b7b5fddecc13246e657a317ee
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 23:37:57 +0800

    Initial commit

---
+++ display_num_with_duplicate.py
@@ -0,0 +1,11 @@
+def prog01():
+
+#Initialize an empty list to store the numbers.
+
+#Loop 10 times to get 10 numbers from the user.
+
+#Add number to the list.
+
+#Initialize an empty list and set to track seen numbers.
+
+#Iterate through the list of numbers.
