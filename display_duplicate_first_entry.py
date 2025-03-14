9a63218417032151fe197750c5ef6eb699e58c65
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 22:55:55 +0800

    Added a loop

--- display_duplicate_first_entry.py
+++ display_duplicate_first_entry.py
@@ -1,7 +1,10 @@
 #Initialize an empty list to store the numbers
-
+def prog02():
+    numbers = []
 #Loop 10 times to get 10 numbers from the user
-
+    for i in range(10):
+        num = int(input(F"Enter number {i + 1}: "))
+        numbers.append(num)
 #Initialize an empty lise to track numbers already printed
 
 #Check if the number has not been printed yet.
