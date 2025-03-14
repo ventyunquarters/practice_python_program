c1c40cc32c2213a4637d9103dd39f191a5e7e1ce
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-15 01:12:43 +0800

    Initial input

---
+++ display_average.py
@@ -0,0 +1,10 @@
+#Initialize an empty list to  store numbers
+
+#Loop indefinitely until an invaalid input is given
+
+#Get input and convert it to float.
+
+#Check if the list of numbers is not empty.
+
+#Calculate the average
+
