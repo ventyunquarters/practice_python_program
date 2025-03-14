b47b978405d2da82d70325b3cdc2dcfe25761bb6
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-15 00:13:21 +0800

    With input number

--- display_highest_num.py
+++ display_highest_num.py
@@ -1,9 +1,15 @@
 def prog03():
 
 #Add an empty list to store the numbers
-
+    numbers = []
+    while True:
+        try:
 #Get an input and convert it to integer
-
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Ending program.")
+        break
 #Check if the list of numbers is not empty
 
 
