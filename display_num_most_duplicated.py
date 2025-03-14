cbfb7fe956f9c53627db4f457250b92e697e0d59
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 23:53:40 +0800

    Loop for input

--- display_num_most_duplicated.py
+++ display_num_most_duplicated.py
@@ -1,7 +1,14 @@
 #Initialize an empty list to store the numbers
-
+def prog02():
+    numbers = []
 #Loop indefinitely until an invalid input is given
-
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Ending program")
+            break
 #Check if the list of numbers is empty
 
 #Initialize dictionary to store the count of each numbers
