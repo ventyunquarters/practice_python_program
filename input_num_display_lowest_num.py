fdb314ae1beef0e61ebc5acc09ded746132efcb6
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 23:22:40 +0800

    With loop til invalid input was given

--- input_num_display_lowest_num.py
+++ input_num_display_lowest_num.py
@@ -1,5 +1,12 @@
 #Initialize an empty list to store the num
-
+def prog04():
+    numbers = []
 #Loop indefinitely until an invalid input is given
-
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            print("Invalid input. Ending program.")
+            break
 #Check if the list of numbers is not empty.
