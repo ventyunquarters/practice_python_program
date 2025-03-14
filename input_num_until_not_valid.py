05a0d2beeed4233bb3452f1e917c74abe53d82d2
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 23:11:04 +0800

    Added input number

--- input_num_until_not_valid.py
+++ input_num_until_not_valid.py
@@ -1,5 +1,13 @@
 #Initialize an empty list to store the numbers
+def prog03():
 
+    numbers = []
+    while True:
+        try:
 #Get input and convert it to an integer
-
+            num = int(input("Enter a number: "))
+            if num in numbers:
+                print("Duplicate")
+            else:
+                numbers.append(num)
 #Handle invalid input
