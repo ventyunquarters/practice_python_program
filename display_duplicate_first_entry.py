2a52cc8927de83c12700873c289d127add15d897
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 22:44:26 +0800

    Initial commit

---
+++ display_duplicate_first_entry.py
@@ -0,0 +1,8 @@
+#Initialize an empty list to store the numbers
+
+#Loop 10 times to get 10 numbers from the user
+
+#Initialize an empty lise to track numbers already printed
+
+#Check if the number has not been printed yet.
+
