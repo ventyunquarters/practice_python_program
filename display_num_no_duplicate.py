2d367110ddf0edb27ece88ed229fafbfe2a890fb
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-14 22:12:08 +0800

    Initial commit.

---
+++ display_num_no_duplicate.py
@@ -0,0 +1,13 @@
+#Initialize an empty list to store numbers
+
+#Loop 10 times to get 10 numbers from user.
+
+#Get input and convert it to integer
+
+#Initialize an empty list
+
+#Iterate through the list of numbers
+
+#Check if the number appears only once in the list.
+
+#Print the list of unique numbers.
