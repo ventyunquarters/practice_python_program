f0a9f0ed5f55f89f8aef349bc03f72f56c824ab4
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-15 00:57:18 +0800

    Initial commit

---
+++ sort_num_highest_to_lowest.py
@@ -0,0 +1,11 @@
+def prog04():
+#Add empty list to store the numbers.
+
+#Add loop until an invalid input is given
+
+#Get input and convert it to an integer.
+
+#Check the list of number is not empty.
+
+#Sort numbers in descending order using function
+
