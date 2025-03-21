32fb95307c0242798f5d71d14d231b7a58ebd2b6
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 23:28:43 +0800

    Final commit with the proper title

--- batch_five_prac_prog/print_program_with_incorrect_casting_to_proper.py
+++ batch_five_prac_prog/print_program_with_incorrect_casting_to_proper.py
@@ -1,7 +1,10 @@
 def prog05():
 
 #Ask user to input their name
-
+    full_name =  input("Enter your full name with incorrect casing: ")
 #Convert the input to proper casing using title method
-
+    proper_name = full_name.title()
 #Print the proper name
+    print(proper_name)
+
+prog05()
