119235a0cdab5badb65b1c59f8b9f8ebc3e6e4b1
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-22 03:23:18 +0800

    With char lower checker

--- batch_five_prac_prog/print_with_reverse_casing.py
+++ batch_five_prac_prog/print_with_reverse_casing.py
@@ -1,15 +1,17 @@
 def prog06():
 
 #Ask user to input their name with incorrect casing
-
+    full=name = input("Enter your full name with incorrect casing: ")
+    reversed_case_name = " "
 #Iterate through each character in the input string
-
+    for char in full_name:
 #Check if the character is lowercase
-
+        if char.islower():
 #Convert to uppercase and append.
-
+            reversed_case_name += char.upper()
 #Check if the character is uppercase.
-
+        elif char.isupper():
+            reversed_case_name += char.lower()
 #Convert to lowercase and append.
 
 #Print reversed case name
