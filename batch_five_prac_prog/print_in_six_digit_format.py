d698ff9b8251e07720046a781efd3b2c7ddc2a98
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 20:14:05 +0800

    Added input number function

--- batch_five_prac_prog/print_in_six_digit_format.py
+++ batch_five_prac_prog/print_in_six_digit_format.py
@@ -1,6 +1,12 @@
 def prog02():
 
 #Ask user to input a number between 0-1000
+    while True:
+        try:
+            num = int(input('Enter a number between 0 to 1000: '))
+            if 0 <= num <= 1000:
+                break
+            else:
 
 #Exit the loop if the input is valid
 
