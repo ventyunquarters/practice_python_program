07416be63a6cc0503a62d361e041ea2f0f3864b8
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 20:40:27 +0800

    Final added upper and fixed the def

--- batch_five_prac_prog/print_name_all_caps.py
+++ batch_five_prac_prog/print_name_all_caps.py
@@ -1,7 +1,9 @@
-prog03():
+def prog03():
 
 #Ask user to enter full name
-
+    full_name = input("Enter your full name: ")
 #Convert input to uppercase using upper method
-
+    capitalized_name = full_name.upper()
 #Print the capitalized name
+    print(capitalized_name)
+prog03()
