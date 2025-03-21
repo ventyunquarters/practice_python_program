56561b7e47bd00e305f6a492d7a77799b22011f8
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 20:45:01 +0800

    Final with lowercase conversion

--- batch_five_prac_prog/print__name_low_caps.py
+++ batch_five_prac_prog/print__name_low_caps.py
@@ -1,7 +1,10 @@
 def prog04():
 #Ask user to enter their full name
-
+    full_name =  input("Enter your full name: ")
 #Convert the input using lowercase method
-
+    lowercase_name = full_name.lower()
 #Print lower case
+    print(lowercase_name)
+
+prog04()
 
