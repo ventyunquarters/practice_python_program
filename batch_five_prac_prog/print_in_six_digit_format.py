460f4db15925b3db27cb001e4df3205d3b905081
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 20:12:00 +0800

    Initial commit

---
+++ batch_five_prac_prog/print_in_six_digit_format.py
@@ -0,0 +1,9 @@
+def prog02():
+
+#Ask user to input a number between 0-1000
+
+#Exit the loop if the input is valid
+
+#Else print the invalid number to enter new one
+
+#Formatt he number as 6 digit string with leading zeros
