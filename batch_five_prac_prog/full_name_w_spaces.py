4fb717a44e97b5d73f1d5c07e87147edd7eaea77
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 19:51:15 +0800

    Final code

--- batch_five_prac_prog/full_name_w_spaces.py
+++ batch_five_prac_prog/full_name_w_spaces.py
@@ -1,7 +1,9 @@
 def prog01():
 
 #Ask the user to input their full name with leading spaces
-
+    full_name = input("Enter your full name with leading spaces: ")
 #Use an lstrip to remove the leading spaces
-
+    trimmed_name = full_name.strip()
 #Print trimmed name
+    print(trimmed_name)
+prog01()
