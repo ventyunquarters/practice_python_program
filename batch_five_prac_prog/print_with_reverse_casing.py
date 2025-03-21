0a437fee571729713082c2512bc149cbc7d2d35d
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-22 03:18:11 +0800

    Initial commit

---
+++ batch_five_prac_prog/print_with_reverse_casing.py
@@ -0,0 +1,16 @@
+def prog06():
+
+#Ask user to input their name with incorrect casing
+
+#Iterate through each character in the input string
+
+#Check if the character is lowercase
+
+#Convert to uppercase and append.
+
+#Check if the character is uppercase.
+
+#Convert to lowercase and append.
+
+#Print reversed case name
+
