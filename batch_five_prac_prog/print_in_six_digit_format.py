b1548484881fcee9041fd9f9c80d87faaec21cec
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-21 20:20:33 +0800

    Finished and tested code.

--- batch_five_prac_prog/print_in_six_digit_format.py
+++ batch_five_prac_prog/print_in_six_digit_format.py
@@ -12,3 +12,6 @@ def prog02():
             print("Invalid input. Please enter a number between 0 to 1000.")
 
 #Format the number as 6 digit string with leading zeros
+    formatted_num = "{:06d}".format(num)
+    print(formatted_num)
+prog02()
