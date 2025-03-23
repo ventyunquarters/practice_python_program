ab9879b985cedcda654db7b63bb7400038bb6cbb
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-24 03:14:01 +0800

    Testing it with words. I love this one so much. Fave code from batch 5. Plus used the functions to split and count the words.

--- batch_five_prac_prog/print_number_of_words_input.py
+++ batch_five_prac_prog/print_number_of_words_input.py
@@ -1,9 +1,10 @@
 def prog07():
 #Ask user to input a complete statement
-
+    statement = input("Enter your statement: ")
 #Using function split we are going to make it into list of words
-
+    words = statement.split()
 #Use lens function to get the number of words
-
+    word_count = len(words)
 #print the word count
-
+    print(word_count)
+prog07()
