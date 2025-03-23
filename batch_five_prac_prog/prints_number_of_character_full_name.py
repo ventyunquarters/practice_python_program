950e56589c9888c69a6cba6a9ff3f167655e301a
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-24 03:22:56 +0800

    Using len function to count it again and character to indicate it was the character per word we will count.

--- batch_five_prac_prog/prints_number_of_character_full_name.py
+++ batch_five_prac_prog/prints_number_of_character_full_name.py
@@ -1,7 +1,9 @@
 def prog08():
 
 #Ask the user to input their full name
-
+    full_name = input("Enter your full name: ")
 #Using char and len we will get the number of characters from the name
-
+    char_count = len(full_name)
 #Print the character count
+    print(char_count)
+prog08()
