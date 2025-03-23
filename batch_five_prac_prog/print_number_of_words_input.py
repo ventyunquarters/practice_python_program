843e8f9495f1fcb08a561ba5e9a92c510a931523
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-24 02:55:07 +0800

    Pseudocode

---
+++ batch_five_prac_prog/print_number_of_words_input.py
@@ -0,0 +1,9 @@
+def prog07():
+#Ask user to input a complete statement
+
+#Using function split we are going to make it into list of words
+
+#Use lens function to get the number of words
+
+#print the word count
+
