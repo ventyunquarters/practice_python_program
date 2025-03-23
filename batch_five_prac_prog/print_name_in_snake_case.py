1125f5dc3314df126dc6531037e34b1ed25a243b
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-24 03:51:41 +0800

    Added a possible outline

---
+++ batch_five_prac_prog/print_name_in_snake_case.py
@@ -0,0 +1,9 @@
+def prog10():
+
+#Ask user to print full name with incorrect casing
+
+#Use the character to see if the character is alphanumeric it would keep it otherwise it would be replaced with a space
+
+#Join the words and add the _ to replace space
+
+#Print snake case
