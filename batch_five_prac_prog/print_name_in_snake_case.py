66037007e1cf7c04f8327302a259f14cb62e95b3
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-24 03:55:28 +0800

    Results. Used the split and lowered the casing to achieve the snake case.

--- batch_five_prac_prog/print_name_in_snake_case.py
+++ batch_five_prac_prog/print_name_in_snake_case.py
@@ -1,9 +1,11 @@
 def prog10():
 
 #Ask user to print full name with incorrect casing
-
+    full_name = input("Enter your name with incorrect casting: ")
 #Use the character to see if the character is alphanumeric it would keep it otherwise it would be replaced with a space
-
+    words = "".join(c if c.isalnum() else "_" for c in full_name).lower().split()
 #Join the words and add the _ to replace space
-
+    snake_case_name = "_".join(words)
 #Print snake case
+    print(snake_case_name)
+prog10()
