9ade8a621325789e1ffe5087bcda722003ea8610
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-28 22:57:57 +0800

    Intial commit with the pseudocode for it

---
+++ batch_six_prac_prog/remove_prefix_ask_user_name.py
@@ -0,0 +1,11 @@
+#Define function to remove the prefix without using the remove_prefix function
+def alt_remove_prefix(input_string, prefix):
+
+    if input_string.startswith(prefix):
+        return input_string[len(prefix):]
+
+#Ask the user to input their name with prefix.
+
+#Remove the prefix
+
+#Display the cleaned name
