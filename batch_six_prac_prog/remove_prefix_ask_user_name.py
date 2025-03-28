b89221534b5f130993d4d294f083f0ba4f5e6767
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-28 23:10:03 +0800

    Set the condition for the prefixes to stick wtih Mr. or Ms.

--- batch_six_prac_prog/remove_prefix_ask_user_name.py
+++ batch_six_prac_prog/remove_prefix_ask_user_name.py
@@ -3,9 +3,12 @@ def alt_remove_prefix(input_string, prefix):
 
     if input_string.startswith(prefix):
         return input_string[len(prefix):]
-
-#Ask the user to input their name with prefix.
-
+    else:
+        return input_string
+#Ask the user to input their name with prefix(optional).
+user_name = input("Enter your name (with optional prefix 'Mr. ' or 'Ms.'): ")
 #Remove the prefix
-
+cleaned_name = alt_remove_prefix(user_name, "Mr. ")
+cleaned_name = alt_remove_prefix(cleaned_name, "Ms. ")
 #Display the cleaned name
+print("Your name without prefix: ", cleaned_name)
