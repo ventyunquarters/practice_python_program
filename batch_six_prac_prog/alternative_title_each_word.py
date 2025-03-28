64349927fa4f90f84481d21c41a9783fe384b900
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-29 03:47:49 +0800

    After using the alpha with same procedure we will ask them to do late night drive instead

--- batch_six_prac_prog/alternative_title_each_word.py
+++ batch_six_prac_prog/alternative_title_each_word.py
@@ -1,3 +1,4 @@
+
 def alternative_title(input_string):
 
     if not input_string:
@@ -7,7 +8,7 @@ def alternative_title(input_string):
     capitalize_next = True #Marker to capitalize the next character"
 
     for char in input_string:
-        if char.isalppha():
+        if char.isalpha():
             if capitalize_next:
                 if 'a' <= char <= 'z':
                     result += chr(ord(char) - 32) #capitalize
@@ -19,3 +20,18 @@ def alternative_title(input_string):
                     result += chr(ord(char) + 32) #Lowercase
                 else:
                     result += char
+        else:
+            result += char
+            capitalize_next = True #Capitalzied after non-alpha
+    return result
+
+#Ask for user input
+user_statement = input("Enter a statement: ")
+
+
+#Title case the string
+title_cased_statement = alternative_title(user_statement)
+
+#Display the results
+print("Title-cased statement:", title_cased_statement)
+
