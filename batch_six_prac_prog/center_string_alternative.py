18bd2fb822b59f54206edc4da1a8d5b3b3e78681
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-29 03:08:16 +0800

    Asked the user for statement. Had to adjust the code a bit.

--- batch_six_prac_prog/center_string_alternative.py
+++ batch_six_prac_prog/center_string_alternative.py
@@ -8,3 +8,13 @@ def custom_center(input_string, length):
     right_padding = " " * (padding_length - len(left_padding))
 
     return left_padding + input_string + right_padding
+
+#Ask user to input something or a statement
+user_statement =  input("Enter your statement: ")
+desired_length = int(input("Enter desired length: "))
+
+#Center the string
+centered_string = custom_center(user_statement, desired_length)
+
+#Display the result and use a marker
+print("Centered string:", centered_string, "|")
