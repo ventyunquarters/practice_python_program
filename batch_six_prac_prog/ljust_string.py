956cf065905ea315634a85fa4b4db2cf6b83c09b
unknown <airaevillarito@iskolarngbayan.pup.edu.ph>
2025-03-29 02:20:23 +0800

    Asked for user input and used a line as a marker for the padding

--- batch_six_prac_prog/ljust_string.py
+++ batch_six_prac_prog/ljust_string.py
@@ -5,3 +5,13 @@ def custom_ljust(input_string, length):
 
     padding = " " * (length - len(input_string))
     return input_string + padding
+
+#Get user input
+user_statement = input("Enter your statement: ")
+desired_length = int(input("Enter desired length: "))
+
+#Ljust the string
+left_justified_string = custom_ljust(user_statement, desired_length)
+
+#display the result and used | as a marker
+print("Left justified string: ", left_justified_string, "|")
